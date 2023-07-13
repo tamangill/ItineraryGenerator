4745c9e34d36e6126566da10b7105de6791cb13d
Taman Gill <tgill0424@gmail.com>
2023-07-13 11:12:59 -0700

    Linted attraction class

--- src/main/java/com/example/ItineraryGenerator/Attraction.java
+++ src/main/java/com/example/ItineraryGenerator/Attraction.java
@@ -1,46 +1,50 @@
+/* Taman Gill Copyright 2023
+ * This class represents a attraction with basic 
+ * information that will be used to display on the 
+ * itinaray. 
+ */
+
 package com.example.ItineraryGenerator;
 
 public class Attraction {
-        private String name;
-        private String address;
-        private double rating;
+    private String name;
+    private String address;
+    private double rating;
         
-        public Attraction(String name, String address, double rating) {
-            this.name = name;
-            this.address = address;
-            this.rating = rating;
-        }
+    public Attraction(String name, String address, double rating) {
+        this.name = name;
+        this.address = address;
+        this.rating = rating;
+    }
+        // Getters and setters for the attributes   
+    public String getName() {
+        return name;
+    }
         
-        // Getters and setters for the attributes
+    public void setName(String name) {
+        this.name = name;
+    }
         
-        public String getName() {
-            return name;
-        }
+    public String getAddress() {
+        return address;
+    }
         
-        public void setName(String name) {
-            this.name = name;
-        }
+    public void setAddress(String address) {
+        this.address = address;
+    }
         
-        public String getAddress() {
-            return address;
-        }
+    public double getRating() {
+        return rating;
+    }
         
-        public void setAddress(String address) {
-            this.address = address;
-        }
-        
-        public double getRating() {
-            return rating;
-        }
-        
-        public void setRating(double rating) {
-            this.rating = rating;
-        }
-    
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     public void displayAttractionDetails() {
         System.out.println("Name: " + name);
         System.out.println("Location: " + address);
         System.out.println("Rating: " + rating);
-        // Add more details specific to the attraction
+
     }
-    }
+}
